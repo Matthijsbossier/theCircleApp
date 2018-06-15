b31ee2d740279e17ce8e99b6ae814b33d77a973d
Matthijs Bossier <mebossie@student.avans.nl>
2018-06-15 12:12:16 +0200

    projecten samengevoegd

---
+++ app/src/main/java/com/example/matthijs/thecircleapp/Constants.java
@@ -0,0 +1,5 @@
+package com.example.matthijs.thecircleapp;
+
+public class Constants {
+    public static final String CHAT_SERVER_URL = "http://188.166.29.146:4000";
+}
---
+++ app/src/main/java/com/example/matthijs/thecircleapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.matthijs.thecircleapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.matthijs.thecircleapp.liveVideoBroadcaster.LiveVideoBroadcasterActivity;
+import com.github.nkzawa.socketio.androidchat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String RTMP_BASE_URL = "rtmp://188.166.29.146/live";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openVideoBroadcaster(View view) {
+        Intent i = new Intent(this, LiveVideoBroadcasterActivity.class);
+        startActivity(i);
+    }
+}
